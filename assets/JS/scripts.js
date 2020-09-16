3c8817aa6db3811a1ae9d175c94aca787104fdc9
Cham Bao <megabloks2010@yahoo.com>
2020-09-17 01:03:43 +0700

    Dit

--- assets/JS/scripts.js
+++ assets/JS/scripts.js
@@ -9,5 +9,25 @@ $(document).ready(function(){
     $('.menu-toggler').click(function(){
         $(this).toggleClass("active");
         $(".header__menu").toggleClass("active");
-    });
+    }); 
+
+    $('.nextBtn').click(function(){
+		var currentImg = $('.active');
+		var nextImg = currentImg.next();
+		
+		if(nextImg.length){
+			currentImg.removeClass('active').css('z-index', -10);
+			nextImg.addClass('active').css('z-index', 10);
+		}
+	});
+	
+	$('.prevBtn').click(function(){
+		var currentImg = $('.active');
+		var prevImg = currentImg.prev();
+		
+		if(prevImg.length){
+			currentImg.removeClass('active').css('z-index', -10);
+			prevImg.addClass('active').css('z-index', 10);
+		}
+	});
 });
